a9e52e18022cbae335fdb4e31e906bfdce88356f
Anthony Nanfito <81890820+ananfito@users.noreply.github.com>
2023-05-30 16:00:04 +0700

    add functions to handle change and submit to the form, create README

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Header from './Header.jsx'
 import Form from './Form.jsx'
 
 function App() {
-
   return (
     <main className='container'>
       <Header />
--- src/Form.jsx
+++ src/Form.jsx
@@ -1,35 +1,82 @@
 import React from 'react'
 
 export default function Form() {
+    const [formData, setFormData] = React.useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: ''
+    })
+  
+    function handleChange(event) {
+        const {name, value} = event.target
+        setFormData(prevFormData => ({
+            ...prevFormData,
+            [name]: value
+        }))
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        console.log(event)
+        const form = event.target
+        const {firstName, lastName, email, password} = formData
+
+        if (firstName != '' && lastName != '' && email != '' && password != '') {
+            console.log('success')
+            
+        } else {
+            console.log('missing form info')
+            switch (firstName === '' | lastName === '' | email === '' | password === '') {
+                case firstName === '':
+                    form.firstName.focus()
+                case lastName === '':
+                    form.lastName.focus()
+                    break;
+                    
+            }
+            
+            
+        }
+    }
+
     return (
         <section className='form-container'>
             <div className='form-container--text'>
                 <p><span className='bold-text'>Try it free 7 days</span> then $20/mo. thereafter</p>
             </div>
-            <form className='form-container--form' action="">
+            <form className='form-container--form' onSubmit={handleSubmit}>
                 <input 
                     type="text" 
-                    name="" 
-                    id="" 
-                    placeholder='First Name' 
+                    name="firstName" 
+                    id="firstName" 
+                    placeholder='First Name'
+                    onChange={handleChange}
+                    value={formData.firstName}
                 />
                 <input 
                     type="text" 
-                    name="" 
-                    id="" 
-                    placeholder='Last Name' 
+                    name="lastName" 
+                    id="lastName" 
+                    placeholder='Last Name'
+                    onChange={handleChange}
+                    value={formData.lastName} 
                 />
                 <input 
                     type="email" 
-                    name="" 
-                    id="" 
-                    placeholder='Email Address' 
+                    name="email" 
+                    id="email" 
+                    placeholder='Email Address'
+                    onChange={handleChange}
+                    value={formData.email} 
                 />
                 <input 
                     type="password" 
-                    name="" 
-                    id="" 
-                    placeholder='Password' 
+                    name="password" 
+                    id="password" 
+                    placeholder='Password'
+                    onChange={handleChange}
+                    value={formData.password} 
                 />
                 <button className='form-container--btn'>
                     Claim your free trial
